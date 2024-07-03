707943dd5d10536c026045f495158bda5a4bdf29
xiaohuo <xiaohuo200@gmail.com>
2024-07-03 15:10:16 +0800

    feat: generate bindings for SequencerInbox and UpgradeExecutor

--- pkg/bindings/gen.go
+++ pkg/bindings/gen.go
@@ -1,3 +1,5 @@
 package bindings
 
 //go:generate abigen --abi RollupCreator.abi --pkg bindings --type RollupCreator --out RollupCreator.go
+//go:generate abigen --abi SequencerInbox.abi --pkg bindings --type SequencerInbox --out SequencerInbox.go
+//go:generate abigen --abi UpgradeExecutor.abi --pkg bindings --type UpgradeExecutor --out UpgradeExecutor.go
