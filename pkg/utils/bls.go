ec849be7f2550e38bab3e8d6cef2e1c7987221a1
xiaohuo <xiaohuo200@gmail.com>
2024-07-03 19:33:29 +0800

    feat: prepare keyset

--- pkg/utils/bls.go
+++ pkg/utils/bls.go
@@ -1,8 +1,13 @@
 package utils
 
 import (
+	"bytes"
 	"encoding/base64"
+	"encoding/binary"
+	"io"
 )
+
+const BinSize = 64 * 1024 // 64 kB
 
 func GenerateBLSKeys() ([]byte, []byte, error) {
 	pubKey, privKey, err := GenerateKeys()
@@ -16,4 +21,58 @@ func GenerateBLSKeys() ([]byte, []byte, error) {
 	encodedPrivKey := make([]byte, base64.StdEncoding.EncodedLen(len(privKeyBytes)))
 	base64.StdEncoding.Encode(encodedPrivKey, privKeyBytes)
 	return encodedPrivKey, encodedPubKey, nil
+}
+
+func PrepareKeyset(pubKeys []string, assumedHonest uint64) (string, error) {
+	var parsedKeys []PublicKey
+	for _, key := range pubKeys {
+		decoded, err := base64.StdEncoding.DecodeString(key)
+		if err != nil {
+			return "", err
+		}
+		p, err := PublicKeyFromBytes(decoded, true)
+		if err != nil {
+			return "", err
+		}
+		parsedKeys = append(parsedKeys, p)
+	}
+	keyset := &DataAvailabilityKeyset{
+		AssumedHonest: uint64(assumedHonest),
+		PubKeys:       parsedKeys,
+	}
+	ksBuf := bytes.NewBuffer([]byte{})
+	if err := keyset.Serialize(ksBuf); err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(ksBuf.Bytes()), nil
+}
+
+type DataAvailabilityKeyset struct {
+	AssumedHonest uint64
+	PubKeys       []PublicKey
+}
+
+func (keyset *DataAvailabilityKeyset) Serialize(wr io.Writer) error {
+	if err := Uint64ToWriter(keyset.AssumedHonest, wr); err != nil {
+		return err
+	}
+	if err := Uint64ToWriter(uint64(len(keyset.PubKeys)), wr); err != nil {
+		return err
+	}
+	for _, pk := range keyset.PubKeys {
+		pkBuf := PublicKeyToBytes(pk)
+		buf := []byte{byte(len(pkBuf) / 256), byte(len(pkBuf) % 256)}
+		_, err := wr.Write(append(buf, pkBuf...))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func Uint64ToWriter(val uint64, wr io.Writer) error {
+	var buf [8]byte
+	binary.BigEndian.PutUint64(buf[:], val)
+	_, err := wr.Write(buf[:])
+	return err
 }
--- pkg/utils/bls_test.go
+++ pkg/utils/bls_test.go
@@ -13,3 +13,27 @@ func TestGenerateBLSKeys(t *testing.T) {
 	fmt.Println("encoded private key: ", string(private))
 	fmt.Println("encoded public key: ", string(public))
 }
+
+func TestPrepareKeyset(t *testing.T) {
+	_, public0, err := GenerateBLSKeys()
+	if err != nil {
+		t.FailNow()
+	}
+	_, public1, err := GenerateBLSKeys()
+	if err != nil {
+		fmt.Println(err.Error())
+		t.FailNow()
+	}
+
+	var publicKeys []string
+	publicKeys = append(publicKeys, string(public0))
+	publicKeys = append(publicKeys, string(public1))
+
+	ret, err := PrepareKeyset(publicKeys, 2)
+	if err != nil {
+		fmt.Println(err.Error())
+		t.FailNow()
+	}
+
+	fmt.Println(string(ret))
+}
